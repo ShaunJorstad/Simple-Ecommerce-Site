4d5f3c443fba5720531c80c48b2f107c793acde1
Luke Donmoyer <lukedonmoyer@gmail.com>
2020-11-19 21:28:47 -0500

    Formatting

--- dataStructures.py
+++ dataStructures.py
@@ -1,38 +1,39 @@
 from wtforms import *
-# from flask_uploads import configure_uploads, IMAGES, UploadSet
-# images = UploadSet('images', IMAGES)
+
 
 class RegistrationForm(Form):
-	email = StringField('Email', [validators.Length(min=6, max=35)])
-	password = PasswordField('New Password', [
-		validators.DataRequired(),
-		validators.EqualTo('confirm', message='Passwords must match')
-	]) 
-	confirm = PasswordField('Confirm Password')
-	accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
-	fname = StringField('fname', [validators.required()])
-	lname = StringField('lname', [validators.required()])
-	submit = SubmitField('Submit')
+    email = StringField('Email', [validators.Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        validators.DataRequired(),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Confirm Password')
+    accept_tos = BooleanField('I accept the TOS', [validators.DataRequired()])
+    fname = StringField('fname', [validators.required()])
+    lname = StringField('lname', [validators.required()])
+    submit = SubmitField('Submit')
+
 
 class LoginForm(Form):
-	email = StringField('Email', [validators.Length(min=6, max=35)])
-	password = PasswordField('Password', [
-		validators.DataRequired(),
-	]) 
-	submit = SubmitField('Submit')
+    email = StringField('Email', [validators.Length(min=6, max=35)])
+    password = PasswordField('Password', [
+        validators.DataRequired(),
+    ])
+    submit = SubmitField('Submit')
 
 
 class User:
-	email = ""
-	password = ""
-	fname = ""
-	lname = ""
+    email = ""
+    password = ""
+    fname = ""
+    lname = ""
 
-	def __init__(self, email, password, fname="", lname=""):
-		self.email = email#
-		self.password = password
-		self.fname = fname
-		self.lname = lname
+    def __init__(self, email, password, fname="", lname=""):
+        self.email = email  #
+        self.password = password
+        self.fname = fname
+        self.lname = lname
+
 
 class Product:
-	pass
+    pass
--- generatePepper.py
+++ generatePepper.py
@@ -1,11 +1,7 @@
-import base64 
-from cryptography.fernet import Fernet 
-from passlib.hash import bcrypt_sha256 
-from passlib.hash import argon2
-import os
+from cryptography.fernet import Fernet
 
 key = Fernet.generate_key()
-pep = Fernet(key)#
+pep = Fernet(key)  #
 # WRITE Pepper
-with open("pepper.bin", 'wb') as fout:
-    fout.write(key)
+with open("pepper.bin", 'wb') as file:
+    file.write(key)
